d8eac4da7681f56e1170d46d7975b4ba82d7be1f
RafliMahesa <muhammadraflimahesa@gmail.com>
2023-09-09 19:28:20 +0700

    Add Bonus Testing

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from main.models import Item
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -8,3 +9,21 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+    
+    def setUp(self):
+        Item.objects.create(name="Obsidian",amount="64",
+                            description="Obsidian is a block found naturally in all dimensions, and created when water flows over a lava source.")
+        Item.objects.create(name="Diamond",amount="32",
+                            description="A diamond is a mineral that can only be obtained from diamond ore, loot chests and suspicious blocks.")
+        
+    def test_items_can_created(self):
+        obsidian = Item.objects.get(name="Obsidian")
+        diamond = Item.objects.get(name="Diamond")
+        self.assertEqual(obsidian.name, "Obsidian")
+        self.assertEqual(diamond.name, "Diamond")
+        self.assertEqual(obsidian.amount, 64)
+        self.assertEqual(diamond.amount, 32)
+
+
+
+    
