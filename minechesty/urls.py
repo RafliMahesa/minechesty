ae94acc98dadc1c86de838e80d8455470497ff4b
RafliMahesa <muhammadraflimahesa@gmail.com>
2023-09-09 21:22:38 +0700

    Update

--- minechesty/urls.py
+++ minechesty/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('main/', include('main.urls')),
