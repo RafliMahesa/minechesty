25b7ef44effbf4dbe541531f92220485458a6bd9
RafliMahesa <muhammadraflimahesa@gmail.com>
2023-09-12 20:08:29 +0700

    Menambah Atribut

--- main/models.py
+++ main/models.py
@@ -4,4 +4,6 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
+    rarity = models.TextField()
+    
 
